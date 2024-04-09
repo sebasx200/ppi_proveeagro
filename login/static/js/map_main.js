93eeebf49af13bc20486d786b5221276fde312aa
sebasx200 <sebasx200@hotmail.com>
2024-04-09 13:09:13 -0500

    the form for adding a supplier is change to have a better look the main map now has a locate plugin to find the user location the base template now has a link to the main map the data for the suppliers is updated and now includes the lat and lon

--- login/static/js/map_main.js
+++ login/static/js/map_main.js
@@ -1,3 +1,4 @@
+// Initialize map
 const map = L.map('map')
 var location_medellin = [6.25184, -75.56359];
 map.setView(location_medellin, 13);
@@ -18,5 +19,18 @@ function onMapClick(e) {
     marker = L.marker(e.latlng).addTo(map);
 }
 
+// Add click event to map
 map.on('click', onMapClick);
+// Add fullscreen control
 map.addControl(new L.Control.Fullscreen());
+// Add locate control
+var lc = L.control
+  .locate({
+    position: "topleft",
+    strings: {
+      title: "Mostrar dónde estoy"
+    }
+  })
+  .addTo(map);
+// Add geocoder control
+L.Control.geocoder().addTo(map);
--- proveeagro/scripts/load_data.py
+++ proveeagro/scripts/load_data.py
@@ -3,10 +3,12 @@ This script loads the data from the suppliers_medellin.xlsx file into the databa
 """
 
 import os
+import sys
 import pandas as pd
 import django
 
 # Add the current directory to the Python path
+sys.path.append(os.getcwd())
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proveeagro.settings')
 django.setup()
@@ -16,12 +18,14 @@ from dashboard.models import Supplier, Location
 # Load the data from the excel file
 df = pd.read_excel('proveeagro/data/suppliers_medellin.xlsx')
 
+# split the coordinates into latitude and longitude
+df[['latitude', 'longitude']] = df['coordinates'].str.split(',', expand=True)
 # Create the locations and suppliers
 
 # this loop iterates over the rows of the dataframe and creates a new location and supplier for each row
 for index, row in df.iterrows():
 
-    location = Location(address=row['address'], city_id=1)
+    location = Location(address=row['address'], latitude=row['latitude'], longitude=row['longitude'], city_id=1)
     location.save()
     supplier = Supplier(name=row['name'], email=row['email'], phone=row['phone'], created_by_id=1, location_id=location.id)
     supplier.save()
