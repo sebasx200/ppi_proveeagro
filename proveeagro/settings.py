521a6edff27c2dc5d607d10682ee5207e0909091
sebasx200 <sebasx200@hotmail.com>
2024-04-25 16:16:23 -0500

    the admin_interface app is installed to style the admin interface

--- proveeagro/settings.py
+++ proveeagro/settings.py
@@ -36,13 +36,16 @@ INSTALLED_APPS = [
     'dashboard',
     'suppliers',
     'farms',
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'colorfield',
 ]
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -111,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 LANGUAGES = [
     ('es', 'Spanish'),
--- suppliers/admin.py
+++ suppliers/admin.py
@@ -3,7 +3,23 @@ from .models import Supplier, Department, City, Location
 
 # Register your models here.
 
-admin.site.register(Supplier)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id','address',)
+    list_editable = ('address',)
+    search_fields = ('address',)
+    list_per_page = 10
+
+admin.site.register(Location, LocationAdmin)
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display=('Supplier','location','phone')
+    ordering=('name',)
+    search_fields = ('name',)
+    list_per_page = 10
+
+    def Supplier(self,obj):
+        return obj.name.upper()
+
 admin.site.register(Department)
 admin.site.register(City)
-admin.site.register(Location)
+admin.site.register(Supplier, SupplierAdmin)
