ac4d3153336490ce75f84cbf46ffec95f1068449
sebasx200 <sebasx200@hotmail.com>
2024-04-28 13:09:44 -0500

    the tables for crop and livestock are created and the models are updated to include the new fields. The admin page is also updated to include them.

--- farms/admin.py
+++ farms/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Farm, FarmType
+from .models import Farm, FarmType, Crop_Type, Crop, Livestock_Type, Livestock
 
 # Register your models here.
 
 admin.site.register(Farm)
 admin.site.register(FarmType)
+admin.site.register(Crop_Type)
+admin.site.register(Crop)
+admin.site.register(Livestock_Type)
+admin.site.register(Livestock)
--- farms/models.py
+++ farms/models.py
@@ -35,3 +35,65 @@ class FarmType(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Crop_Type(models.Model):
+    """
+    this class is used to create a new table in the database for crop types with the following fields
+    """
+    name = models.CharField(max_length=100, verbose_name='Crop Type')
+
+    class Meta:
+        verbose_name = 'Crop Type'
+        verbose_name_plural = 'Crop Types'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+class Crop(models.Model):
+    """
+    this class is used to create a new table in the database for crops with the following fields
+    """
+    name = models.CharField(max_length=100, verbose_name='Crop Name')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated At')
+    crop_type = models.ForeignKey(Crop_Type, on_delete=models.CASCADE, verbose_name='Crop Type')
+
+    class Meta:
+        verbose_name = 'Crop'
+        verbose_name_plural = 'Crops'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+class Livestock_Type(models.Model):
+    """
+    this class is used to create a new table in the database for livestock types with the following fields
+    """
+    name = models.CharField(max_length=100, verbose_name='Livestock Type')
+
+    class Meta:
+        verbose_name = 'Livestock Type'
+        verbose_name_plural = 'Livestock Types'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+class Livestock(models.Model):
+    """
+    this class is used to create a new table in the database for livestock with the following fields
+    """
+    name = models.CharField(max_length=100, verbose_name='Livestock Name')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated At')
+    livestock_type = models.ForeignKey(Livestock_Type, on_delete=models.CASCADE, verbose_name='Livestock Type')
+
+    class Meta:
+        verbose_name = 'Livestock'
+        verbose_name_plural = 'Livestocks'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
