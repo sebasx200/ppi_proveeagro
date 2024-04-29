dd31259363d8546cdc442fa4b8e8d3461db29040
sebasx200 <sebasx200@hotmail.com>
2024-04-29 14:26:08 -0500

    the tables for farms are updated and the migrations are created and are added to the admin page

--- farms/admin.py
+++ farms/admin.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
-from .models import Farm, FarmType, Crop_Type, Crop, Livestock_Type, Livestock
+from .models import Farm, Farm_Type, CropOrLivestock_Type, CropOrLivestock
 
 # Register your models here.
 
-admin.site.register(Farm)
-admin.site.register(FarmType)
-admin.site.register(Crop_Type)
-admin.site.register(Crop)
-admin.site.register(Livestock_Type)
-admin.site.register(Livestock)
+class FarmAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at', 'user', 'location', 'farm_type')
+    search_fields = ('name', 'user', 'location', 'farm_type')
+    list_filter = ('created_at', 'updated_at', 'user', 'location', 'farm_type')
+    list_per_page = 10
+
+class Farm_TypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_per_page = 10
+
+class CropOrLivestock_TypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'farm_type')
+    search_fields = ('name', 'farm_type')
+    list_filter = ('farm_type',)
+    list_per_page = 10
+
+class CropOrLivestockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('created_at', 'updated_at')
+    list_per_page = 10
+
+admin.site.register(Farm, FarmAdmin)
+admin.site.register(Farm_Type, Farm_TypeAdmin)
+admin.site.register(CropOrLivestock_Type, CropOrLivestock_TypeAdmin)
+admin.site.register(CropOrLivestock, CropOrLivestockAdmin)
