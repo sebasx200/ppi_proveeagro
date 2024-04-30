a74747f23064b43ee3e8401e133299f0b1137307
sebasx200 <sebasx200@hotmail.com>
2024-04-29 21:29:33 -0500

    the admin models are updated solving some issues with the related fields

--- farms/admin.py
+++ farms/admin.py
@@ -4,26 +4,26 @@ from .models import Farm, Farm_Type, CropOrLivestock_Type, CropOrLivestock
 # Register your models here.
 
 class FarmAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at', 'updated_at', 'user', 'location', 'farm_type')
-    search_fields = ('name', 'user', 'location', 'farm_type')
-    list_filter = ('created_at', 'updated_at', 'user', 'location', 'farm_type')
+    list_display = ['name', 'location', 'farm_type',]
+    search_fields = ['name', 'location__address', 'farm_type__name',]
+    list_filter = ['location__city__name', 'location__city__department__name', 'farm_type__name',]
     list_per_page = 10
 
 class Farm_TypeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ['name',]
+    search_fields = ['name',]
     list_per_page = 10
 
 class CropOrLivestock_TypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'farm_type')
-    search_fields = ('name', 'farm_type')
-    list_filter = ('farm_type',)
+    list_display = ['name',]
+    search_fields = ['name',]
+    list_filter = ['name', 'farm_type__name',]
     list_per_page = 10
 
 class CropOrLivestockAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at', 'updated_at')
-    search_fields = ('name',)
-    list_filter = ('created_at', 'updated_at')
+    list_display = ['name', 'crop_or_livestock_type',]
+    search_fields = ['name', 'crop_or_livestock_type__name',]
+    list_filter = ['crop_or_livestock_type__name',]
     list_per_page = 10
 
 admin.site.register(Farm, FarmAdmin)
