651be0022d5ee934042aae8100db7cd986ea43aa
sebasx200 <sebasx200@hotmail.com>
2024-05-03 23:06:59 -0500

    the serializer for farms is updated to include the location of the farm

--- farms/serializer.py
+++ farms/serializer.py
@@ -1,7 +1,29 @@
 from rest_framework import serializers
-from .models import Farm, Farm_Type, CropOrLivestock_Type, CropOrLivestock
+from .models import Farm
+from suppliers.models import Location
+from suppliers.serializer import LocationSerializer
 
 class FarmSerializer(serializers.ModelSerializer):
+
+    location = LocationSerializer()
+
     class Meta:
         model = Farm
-        fields = '__all__'
+        fields = ['id','name', 'user', 'location', 'farm_type']
+
+    def create(self, validated_data):
+        location_data = validated_data.pop('location')
+        location = Location.objects.create(**location_data)
+        farm = Farm.objects.create(location=location, **validated_data)
+        return farm
+    
+    def update(self, instance, validated_data):
+        location_data = validated_data.pop('location')
+        location = instance.location
+        instance.name = validated_data.get('name', instance.name)
+        instance.user = validated_data.get('user', instance.user)
+        location.address = location_data.get('address', location.address)
+        location.city = location_data.get('city', location.city)
+        instance.save()
+        location.save()
+        return instance
