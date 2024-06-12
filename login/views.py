93ef85d09f9832ca1eb903c888e5e9087bb9d1d2
sebasx200 <sebasx200@hotmail.com>
2024-06-12 04:33:03 -0500

    now the user can change its data

--- login/views.py
+++ login/views.py
@@ -22,3 +22,10 @@ class UserView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+    
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
